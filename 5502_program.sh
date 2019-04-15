25fec4c29d55628eb1476d76b96f0bd906bde3d5
Lee Crane <rlcrane22@gmail.com>
2019-04-14 19:03:00 -0500

    Update comments and added test files

--- 5502_program.sh
+++ 5502_program.sh
@@ -2,7 +2,7 @@
 #This script will take a url to a file create a directory with the file name then break down the file by cleaning it and 
 # counting the number of times a word is in the file.
 
-#if check to ee if the user type in help
+#if check to see if the user type in help
 if [ "$1" = "--help" ] ;
 	then
 		echo "For this script to work all the pdfs or txt files must exist in a files directory."
@@ -55,7 +55,7 @@ if [ -d "section3/ngramFiles" ]; then
 fi
 	mkdir "section3/ngramFiles"
 
-#All files must be in the files folder
+#All files must be in the files folder. For the first 3 sections we will loop over all files to be index.
 for file in files/*;
 do
 #Section 1 the files must be in the files directory or we will wget to download the files and move them to section 1 directory.------------------------------------------------
@@ -91,55 +91,65 @@ cat  "section2/files/$currFileName.txt" | tr '.' '\n' | sed $'s/[^[:print:]\t]//
 
 
 #Section 3 Create ngrams by punctuation.-----------------------------------------------------------------------------------------------------------------
+#just coping over the files from the previous step for saving.
 cp "section2/cleanFiles/$currFileName.txt" "section3/cleanFiles/$currFileName.txt"
 cp "section3/cleanFiles/$currFileName.txt" "section3/tmp1.txt"
+
+#Here is where we take the file and rotor all the lies by 2, 3 or 4 for building the ngrams
 tail +2 "section3/cleanFiles/$currFileName.txt" > "section3/tmp2.txt"
 tail +3 "section3/cleanFiles/$currFileName.txt" > "section3/tmp3.txt"
 tail +4 "section3/cleanFiles/$currFileName.txt" > "section3/tmp4.txt"
 
-
+#now we have the ngram files build here is where we use paste to bring them back together.
 paste -d ' ' "section3/tmp1.txt"> "section3/1-grams.txt"
 paste -d ' ' "section3/tmp1.txt" "section3/tmp2.txt" > "section3/2-grams.txt"
 paste -d ' ' "section3/tmp1.txt" "section3/tmp2.txt" "section3/tmp3.txt" > "section3/3-grams.txt"
 paste -d ' ' "section3/tmp1.txt" "section3/tmp2.txt" "section3/tmp3.txt" "section3/tmp4.txt" > "section3/4-grams.txt"
 
-
+#once we have the ingram files these lines are used to remove the lines that do not meet the ngram length requirment as we are breaking it by each period.
 cat "section3/1-grams.txt" | sed 's/|||*//' | awk '{ print NF, $0 }'| awk '$1 != "1" { next } { print }' | colrm 1 2 > section3/ngramFiles/"$currFileName"_1-gram.txt
 cat "section3/2-grams.txt" | sed 's/|||*//' | awk '{ print NF, $0 }'| awk '$1 != "2" { next } { print }' | colrm 1 2 > section3/ngramFiles/"$currFileName"_2-gram.txt
 cat "section3/3-grams.txt" | sed 's/|||*//' | awk '{ print NF, $0 }'| awk '$1 != "3" { next } { print }' | colrm 1 2 > section3/ngramFiles/"$currFileName"_3-gram.txt
 cat "section3/4-grams.txt" | sed 's/|||*//' | awk '{ print NF, $0 }'| awk '$1 != "4" { next } { print }' | colrm 1 2 > section3/ngramFiles/"$currFileName"_4-gram.txt
 
- #echo "$file";
 done
 
 #Section 4 Create ngrams by frequencies.----------------------------------------------------------------------------------------------------------------
 
+#just creating some folders for section 4
 if [ -d "section4/ngramFreqFiles" ]; then 
 	rm -Rf "section4/ngramFreqFiles"; 
 fi
 	mkdir "section4/ngramFreqFiles"
 
+#coping over data from previous step.
 cp -R section3/ngramFiles/ section4/ngramFiles/
+
+# here we are looping over the ingram files to get the frequencies count
 for file in section4/ngramFiles/*;
 do
 #get the current file name for this loop
 currFile=$(basename "$file")
 currFileName=`echo $currFile | cut -d\. -f1`;
 
+#now we will sort the file and count the unique values.
 sort "$file" | uniq -c | sort -nr > section4/ngramFreqFiles/"$currFileName"_withFreq.txt
 
 done
 
 #Section 5 Create index file for ngrams.----------------------------------------------------------------------------------------------------------------
 
+#coping over data from previous step.
 cp -R section4/ngramFiles/ section5/ngramFiles/
 
+#here we will build the index file of all the ingrams. We first start by looping over all the files then adding the uniq values to the index file in section 5.
 for file in section5/ngramFiles/*;
 do
 #get the current file name for this loop
 currFile=$(basename "$file")
 currFileName=`echo $currFile | cut -d\. -f1`;
 origFileName=${currFileName::-7}.pdf
+
 
 cat "$file" | awk -v var="$origFileName" '{print $0, "\x27"var"\x27"}' | sed -e "s/ '/ :'/g" | awk '!seen[$0]++' >> section5/index.txt 
 
@@ -150,6 +160,7 @@ done
 #copy index from sec5 to sec6 as staging file
 cp section5/index.txt section6/indexStage.txt
 
+#datamash is the process of merging all the file names that have the same index value.
 datamash -t: -s -g 1 collapse 2 < section6/indexStage.txt > section6/index.txt
 
 
